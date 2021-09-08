a10888020eb898b779f11f14a094b879c421fa49
ongian <ongchristian117@gmail.com>
2021-09-08 09:14:32 +0800

    cabinet code validation added

--- src/components/CabinetLock/CabinetLock.js
+++ src/components/CabinetLock/CabinetLock.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Lock from './Lock/Lock';
 import style from './Cabinet.module.css';
+
 const CabinetLock = (props) => {
+    const [isValid, setIsValid] = useState(false)
     const cabinetCode = [
-        {letter: 'R', number: 3}, 
-        {letter: 'A', number: 6}, 
-        {letter: 'I', number: 2}, 
-        {letter: 'N', number: 9}, 
-        {letter: 'B', number: 5}, 
-        {letter: 'O', number: 9}, 
-        {letter: 'W', number: 8}
+        {letter: 'R', number: 3, valid: false}, 
+        {letter: 'A', number: 6, valid: false}, 
+        {letter: 'I', number: 2, valid: false}, 
+        {letter: 'N', number: 9, valid: false}, 
+        {letter: 'B', number: 5, valid: false}, 
+        {letter: 'O', number: 9, valid: false}, 
+        {letter: 'W', number: 8, valid: false}
     ];
-    const LockArr = cabinetCode.map(cab => (
-        <Lock secretCode={cab.number} key={cab.letter} placeholder={cab.letter} />
-    ))
+    
+    const validateCode = (code, index) => {
+        if(code === cabinetCode[index].number){
+            cabinetCode[index].valid = true
+        } else {
+            cabinetCode[index].valid = false
+        }
+    }
+    let LockArr;
+    if(isValid) {
+        LockArr = <p>Congratulations, you found another clue!</p>;
+    } else {
+        LockArr = cabinetCode.map((cab, ind) => (
+            <Lock validateCode={validateCode} codeOrder={ind} secretCode={cab.number} key={cab.letter} placeholder={cab.letter} />
+        ))
+    }
+
+    const unlock = () => {
+        if(cabinetCode.every(cab => cab.valid)){
+            setIsValid(true)
+        } else {
+            setIsValid(false)
+        }
+        console.log(cabinetCode)
+    }
+    
     return <div className={style.cabinetLock}>
         {LockArr}
-        <button className={style.submit}>Submit</button>
+        <button className={style.submit} onClick={unlock}>Submit</button>
     </div>;
 }
  
--- src/components/CabinetLock/Lock/Lock.js
+++ src/components/CabinetLock/Lock/Lock.js
@@ -1,45 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import style from './Lock.module.css';
 
 const Lock = (props) => {
     const [code, setCode] = useState(null);
-    const [match, setMatch] = useState(false)
+    //const [match, setMatch] = useState(false);
+    useEffect(() => {
+        props.validateCode(code, props.codeOrder);
+    }, [code, props])
     const Increment = () => {
         if(code === 9){
             setCode(0)
         } else {
             setCode(code + 1)
         }
-        if(props.secretCode === code){
-            setMatch(true)
-        }
-        matchNum()
     }
     const Decrement = () => {
-        if(code <= 0 || code === ''){
+        if(code <= 0 || code === props.placeholder){
             setCode(9)
         } else {
             setCode(code - 1)
         }
-        if(props.secretCode === code){
-            setMatch(true)
-        }
-        matchNum()
     }
-    const InputVal = (e) => {
-        setCode(e.target.value)
-        matchNum()
-    }
-    const matchNum = () => {
-        const recentCode = code;
-        if(props.secretCode === recentCode){
-            setMatch(true)
-        }
-    }
-    console.log(match)
+
+    // const passCode = () => {
+    //     props.getCode(code)
+    // }
+    //console.log(match)
     return <div className={style.lock}>
         <input type="button" onClick={Increment} className={style.plus} />
-        <input type="tel" value={code} placeholder={props.placeholder} onChange={InputVal} className={style.code} defaultValue="" />
+        <span>{code === null ? props.placeholder : code }</span>
         <input type="button" onClick={Decrement} className={style.minus} />
     </div>;
 }
